9f61c478be0297954ac4de99a1c4c202407131cf
sajjad <sajjad.sisakhtpour@gmail.com>
2024-01-27 08:34:36 +0330

    animation, move in horizontal

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ let screen = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
+let c = canvas.getContext("2d");
 canvas.width = screen.width;
 canvas.height = screen.height;
-let c = canvas.getContext("2d");
 
-c.arc(100,100,20,0,2*Math.PI,false)
-c.fillStyle='red'
-c.fill()
+let x = 20;
+dx = 0.5 * 1;
+
+function animate() {
+  c.beginPath();
+  c.clearRect(0, 0, screen.width, screen.height);
+  c.arc(x, 100, 20, 0, 2 * Math.PI, false);
+  c.fillStyle = "red";
+  c.fill();
+  x += dx;
+  requestAnimationFrame(animate);
+}
+
+animate();
