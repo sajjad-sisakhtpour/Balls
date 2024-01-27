8bfbe6a8b9bbdbaba44e453c4f6900e8c4c409b8
sajjad <sajjad.sisakhtpour@gmail.com>
2024-01-27 07:47:51 +0330

    create a canvas

---
+++ script.js
@@ -0,0 +1,8 @@
+let canvas = document.querySelector("canvas");
+let screen = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+canvas.width = screen.width;
+canvas.height = screen.height;
