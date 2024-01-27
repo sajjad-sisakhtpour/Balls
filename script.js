796113ce8dee88b16184d8fdbd9d5f346da18efa
sajjad <sajjad.sisakhtpour@gmail.com>
2024-01-27 09:25:23 +0330

    random  x,y,v & color

--- script.js
+++ script.js
@@ -8,16 +8,18 @@ canvas.width = screen.width;
 canvas.height = screen.height;
 
 let r = 20;
-let x = 20;
-let y = 20;
-dx = 0.5 * 1;
-dy = 0.5 * 1;
+let x = rnd(0+r,screen.width-r);
+let y = rnd(0+r,screen.height-r);
+let color=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+dx = (Math.random()-0.5) * 1;
+dy = (Math.random()-0.5) * 1;
+
 
 function animate() {
   c.beginPath();
   c.clearRect(0, 0, screen.width, screen.height);
   c.arc(x, y, r, 0, 2 * Math.PI, false);
-  c.fillStyle = "red";
+  c.fillStyle = color;
   c.fill();
   x += dx;
   if (x + r > screen.width || x - r < 0) {
@@ -33,5 +35,5 @@ function animate() {
 animate();
 
 function rnd(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
