e5f0d5dcd199322fdb89663169b29e14cd3cbb35
sajjad <sajjad.sisakhtpour@gmail.com>
2024-01-27 08:26:13 +0330

    create a ball

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let screen = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-
 canvas.width = screen.width;
 canvas.height = screen.height;
+let c = canvas.getContext("2d");
+
+c.arc(100,100,20,0,2*Math.PI,false)
+c.fillStyle='red'
+c.fill()
